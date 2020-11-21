574d9112e7cac84a74118ba224ea79173f9fd119
mounvip1525 <mounvip1525@gmail.com>
2020-11-21 12:05:08 +0530

    sent an attachment with mail

--- send_mailtxt.py
+++ send_mailtxt.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 from_email='mounvip1525@gmail.com'
 to_email='mounvi.podapati2019@vitstudent.ac.in'
@@ -15,7 +17,16 @@ msg['Subject']=subject
 #body
 body='Hey there! Sending message with Python!'
 msg.attach(MIMEText(body,'plain'))
-message=msg.as_string()
+
+my_file=open('scrape.csv','rb')
+
+part=MIMEBase('application','octet-stream') #A MIME attachment with the content type "application/octet-stream" is a binary file. Typically, it will be an application or a document that must be opened in an application, such as a spreadsheet or word processor.
+part.set_payload((my_file).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition','attachment; filename= ' + 'scrape.csv')
+msg.attach(part)
+ 
+message=msg.as_string() #attaching the heaer,body and attachment and converting the whole into a string
 
 #body='<b>hey there, sending a bold html tag message'
 #msg.attach(MIMEText(body,'html'))
